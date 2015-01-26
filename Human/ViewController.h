f59c922e3e715b55a1b62c6f6ce78da25b8dd1fa
Brian Hoang <brianhoang@Brians-MBP.attlocal.net>
2015-01-25 23:37:02 -0800

    Initial Commit

---
+++ Human/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Human
+//
+//  Created by Brian Hoang on 1/25/15.
+//  Copyright (c) 2015 Brian Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
